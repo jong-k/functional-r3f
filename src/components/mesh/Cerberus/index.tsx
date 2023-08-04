658a822b74e946d71c5afd80984d539228d85b28
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-08-04 10:27:51 +0900

    feat: 케르베로스 오브젝트 클릭하면 텍스쳐 변경되게 구현

--- src/components/mesh/Cerberus/index.tsx
+++ src/components/mesh/Cerberus/index.tsx
@@ -1,12 +1,20 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Mesh } from "three";
 import { useTexture } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
+const TEXTURE_LIST = ["A", "M", "N", "R", "RM"] as const;
+const GET_TEXTURE_PATH = (style: string) =>
+  `src/assets/obj/cerberus/Cerberus_${style}.jpg`;
+
 export default function Cerberus() {
+  const [currentTextureIdx, setCurrentTextureIdx] = useState(0);
+  const handleClick = () => {
+    setCurrentTextureIdx((prev) => (prev + 1) % TEXTURE_LIST.length);
+  };
   const obj = useLoader(OBJLoader, "src/assets/obj/cerberus/Cerberus.obj");
-  const texture = useTexture("src/assets/obj/cerberus/Cerberus_R.jpg");
+  const texture = useTexture(GET_TEXTURE_PATH(TEXTURE_LIST[currentTextureIdx]));
 
   const geometry = useMemo(() => {
     let g;
@@ -20,7 +28,7 @@ export default function Cerberus() {
   }, [obj]);
 
   return (
-    <mesh geometry={geometry}>
+    <mesh geometry={geometry} onClick={handleClick}>
       <meshStandardMaterial map={texture} />
     </mesh>
   );
